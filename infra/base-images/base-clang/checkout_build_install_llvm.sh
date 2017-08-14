9c61dbf5e35cb92ff6a3298e07543d66d9e3642e
boehme <marcel.boehme@acm.org>
2017-08-14 19:52:40 +0800

    Fix typo

--- infra/base-images/base-clang/checkout_build_install_llvm.sh
+++ infra/base-images/base-clang/checkout_build_install_llvm.sh
@@ -29,11 +29,11 @@ cd clang
 LLVM_REVISION=$(grep -Po "CLANG_REVISION = '\K\d+(?=')" scripts/update.py)
 echo "Using LLVM revision: $LLVM_REVISION"
 
-cd $SRC && svn co http://llvm.org/svn/llvm-project/llvm/trunk@$LLVM_REVISION llvm
-cd $SRC/llvm/tools && svn co http://llvm.org/svn/llvm-project/cfe/trunk@$LLVM_REVISION clang
-cd $SRC/llvm/projects && svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk@$LLVM_REVISION compiler-rt
-cd $SRC/llvm/projects && svn co http://llvm.org/svn/llvm-project/libcxx/trunk@$LLVM_REVISION libcxx
-cd $SRC/llvm/projects && svn co http://llvm.org/svn/llvm-project/libcxxabi/trunk@$LLVM_REVISION libcxxabi
+cd $SRC && git clone http://llvm.org/git/llvm.git
+cd $SRC/llvm/tools && git clone http://llvm.org/git/clang.git 
+cd $SRC/llvm/projects && git clone http://llvm.org/git/compiler-rt.git
+cd $SRC/llvm/projects && git clone http://llvm.org/git/libcxx.git
+cd $SRC/llvm/projects && git clone http://llvm.org/git/libcxxabi.git 
 
 # Build & install
 mkdir -p $WORK/llvm
